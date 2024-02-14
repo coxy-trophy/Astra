0276d26832530cf04fa2c856a0a71a1a531ea413
cadillac-cyrus <myredevesuper@gmail.com>
2024-02-14 08:21:14 -0800

    commit

--- astra-dev/src/pages/login.js
+++ astra-dev/src/pages/login.js
@@ -1,9 +1,9 @@
 import Navbar from "@/components/Navbar";
 import { submitVerificationCode } from "@/network";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
+// import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 import { sendVerificationCode } from "../network";
